4ab055e53f4f6112c2db5a70f846a0cfd6c7bbed
kuldik <msma120@bk.ru>
2023-01-15 14:35:52 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[questions](questions\15.01_return.js)
+[1. basics js](lessons\base.js)
---
+++ questions/15.01_return.js
@@ -0,0 +1,15 @@
+
+// function calc (a, b) {
+//         const c = 12
+//         console.log(c)
+//         return 'asd ' + 'asf';
+// };
+// calc(1, 2);
+
+// const calcArrow = (a, b) => a + b;
+// calcArrow(1, 2);
+
+const calcArrow = (a, b) => {
+    return a + b;
+};
+calcArrow(1, 2);
